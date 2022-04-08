cdebe22267c5780d4b3fc510b217db5ff8897a8e
gustavnilsson14 <gustavnilsson14@gmail.com>
2022-04-09 01:19:16 +0200

    added some textures

--- js/mapicons/icons.js
+++ js/mapicons/icons.js
@@ -1,24 +1,20 @@
-function getIcons(){
-    return [
-        'house1.png',
-        'fort1.png',
-        'radar1.png',
-        'ruin1.png',
-        'ruin2.png',
-        'shop.png',
-        'shop2.png',
-        'skyscraper.png',
-        'skyscraper2.png',
-        'town_hall1.png',
-        'warehouse1.png',
-        'warehouse2.png',
-        'warehouse3.png',
-    ];
+function getIcons() {
+  return [
+    "house1.png",
+    "fort1.png",
+    "radar1.png",
+    "ruin1.png",
+    "ruin2.png",
+    "shop.png",
+    "shop2.png",
+    "skyscraper.png",
+    "skyscraper2.png",
+    "town_hall1.png",
+    "warehouse1.png",
+    "warehouse2.png",
+    "warehouse3.png",
+  ];
 }
-function getTextures(){
-    return [
-        'gravel.jpg',
-        'desert.jpg',
-        'zone.jpg',
-    ];
+function getTextures() {
+  return ["gravel.jpg", "desert.jpg", "zone.jpg", "dirt.jpg", "pavement.jpg", "wasteland.jpg"];
 }
